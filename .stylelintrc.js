f3f3f13e658e4cc4690cdfad4be178c712fd5ee0
andreysmirnov <iadisey@gmail.com>
2022-07-24 16:54:19 +0300

    - add rxjs observable-hooks - fix .stylelintrc rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
+  extends: ['stylelint-prettier/recommended', 'stylelint-config-idiomatic-order', 'stylelint-config-standard', 'stylelint-order-config-standard'],
   plugins: ['stylelint-prettier', 'stylelint-order'],
-  extends: ['stylelint-prettier/recommended', 'stylelint-config-idiomatic-order'],
   rules: {
     'prettier/prettier': true,
+    'selector-class-pattern': '^[a-z][a-zA-Z0-9]+$',
     'color-no-invalid-hex': true,
-    'order/order': ['custom-properties', 'dollar-variables', 'declarations', 'rules', 'at-rules'],
+    'color-hex-case': 'upper',
+    'at-rule-no-unknown': null,
+    'order/order': ['custom-properties', 'dollar-variables', 'declarations', 'rules', 'at-rules']
   },
 };
+
